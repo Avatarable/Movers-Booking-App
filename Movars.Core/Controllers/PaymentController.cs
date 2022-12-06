9bf644aac836a951d557864536ddfae95aabec00
Able <avatarable@gmail.com>
2022-12-06 04:10:26 +0100

    added payment view

---
+++ Movars.Core/Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movars.Core.Models.ViewModels;
+
+namespace Movars.Core.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public PaymentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index(string id)
+        {
+            var payment = new PaymentModel
+            {
+                merchant_code = _configuration.GetValue<string>("QuickTeller:MerchantCode")
+            };
+            return View();
+        }
+    }
+}
---
+++ Movars.Core/Models/ViewModels/PaymentModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movars.Core.Models.ViewModels
+{
+    public class PaymentModel
+    {
+        [Required]
+        public string txn_ref { get; set; }
+
+        [Required]
+        public string merchant_code { get; set; }
+
+        [Required]
+        public string pay_item_id { get; set; }
+
+        [Required]
+        public decimal amount { get; set; }
+
+        [Required]
+        public string currency { get; set; }
+
+        [Required]
+        public string site_redirect_url { get; set; }
+
+        [Required]
+        public string display_mode { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+        public string? Address2 { get; set; }
+    }
+}
